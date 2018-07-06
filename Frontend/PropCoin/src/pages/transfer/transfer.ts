cb3135018e6182c4dd41907b5cd5d6ac8a260a35
Peter Yeung <ypspak@gmail.com>
2018-07-07 04:06:04 +0800

    Add the blank transfer pages

---
+++ Frontend/PropCoin/src/pages/transfer/transfer.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { IonicPage, MenuController, NavController, Platform } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-transfer',
+  templateUrl: 'transfer.html'
+})
+export class TransferPage {
+
+  constructor(public navCtrl: NavController, public menu: MenuController, public platform: Platform) {
+   
+  }
+}
