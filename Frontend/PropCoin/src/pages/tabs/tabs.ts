f0f48c8ee4dc45bb59669d02139ad18f27a336d2
Peter Yeung <ypspak@gmail.com>
2018-07-07 02:02:26 +0800

    Rename from page-tabs to nav-tabs for clarity

--- Frontend/PropCoin/src/pages/tabs/tabs.ts
+++ Frontend/PropCoin/src/pages/tabs/tabs.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
 import { IonicPage, NavController } from 'ionic-angular';
 
 import { Tab1Root, Tab2Root, Tab3Root } from '../';
 
 @IonicPage()
 @Component({
-  selector: 'page-tabs',
+  selector: 'nav-tabs',
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
