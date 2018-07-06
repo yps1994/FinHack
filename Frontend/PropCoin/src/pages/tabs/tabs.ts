18465a7c786a894bb62e85717cc208a4d0807fa7
Peter Yeung <ypspak@gmail.com>
2018-07-07 07:21:40 +0800

    Add 4th tabs and update icon

--- Frontend/PropCoin/src/pages/tabs/tabs.ts
+++ Frontend/PropCoin/src/pages/tabs/tabs.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController } from 'ionic-angular';
 
-import { Tab1Root, Tab2Root, Tab3Root } from '../';
+import { Tab1Root, Tab2Root, Tab3Root, Tab4Root } from '../';
 
 @IonicPage()
 @Component({
@@ -12,14 +12,17 @@ export class TabsPage {
   tab1Root: any = Tab1Root;
   tab2Root: any = Tab2Root;
   tab3Root: any = Tab3Root;
+  tab4Root: any = Tab4Root;
 
   tab1Title = " ";
   tab2Title = " ";
   tab3Title = " ";
+  tab4Title = " ";
 
   constructor(public navCtrl: NavController) {
       this.tab1Title = 'Balance';
       this.tab2Title = 'Transfer';
       this.tab3Title = 'Wallet';
+      this.tab4Title = 'Exchange';
   }
 }
