0dcfbc8abe5d803721b78bf046cd8e45ada7fe19
Peter Yeung <ypspak@gmail.com>
2018-07-07 20:53:21 +0800

    Complete walletPages.

--- Frontend/PropCoin/src/pages/wallet/wallet.ts
+++ Frontend/PropCoin/src/pages/wallet/wallet.ts
@@ -8,6 +8,12 @@ export interface BalanceData {
   balance: number;
 }
 
+export interface TransactionData {
+  date: string;
+  type: string;
+  amount: number;
+}
+
 @IonicPage()
 @Component({
   selector: 'page-wallet',
@@ -16,22 +22,69 @@ export interface BalanceData {
 
 export class WalletPage {
 
-  balanceData: BalanceData;
+  walletRegion: string;
 
+  balanceData: BalanceData;
+ 
+  numTransactionProcessed: number;
+  transactionData: TransactionData[];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+
     this.balanceData = {
-      region: "Hong Kong Island",
+      region: "HK Island",
       coins: 5.006,
       balance: 901195
     };
+
+    this.transactionData = [
+    {
+      date: "4/7",
+      type: "Buy",
+      amount: 1,
+    },
+    {
+      date: "3/7",
+      type: "Buy",
+      amount: 1,
+    },
+    {
+      date: "3/7",
+      type: "Withdraw",
+      amount: 1,
+    },
+    {
+      date: "2/7",
+      type: "Buy",
+      amount: 1,
+    },
+    {
+      date: "4/6",
+      type: "Buy",
+      amount: 1,
+    },
+    {
+      date: "3/5",
+      type: "Buy",
+      amount: 1,
+    },
+    {
+      date: "7/4",
+      type: "Withdraw",
+      amount: 1,
+    },
+    {
+      date: "2/4",
+      type: "Interest",
+      amount: 0.01
+    }];
+  }
 
     /*
     this.balanceData.region = navParams.get('region');
     this.balanceData.coins = navParams.get('coins');
     this.balanceData.balance = navParams.get('balance');
     */
-  }
 
   PPCToHKD(sellHKDValue) {
     if (isNaN(sellHKDValue)) return 0;
