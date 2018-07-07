75e391c33db895b47afcc0b124f771b3be554c14
Peter Yeung <ypspak@gmail.com>
2018-07-07 13:23:08 +0800

    Remove all unused assets

--- Frontend/PropCoin/src/app/app.component.ts
+++ Frontend/PropCoin/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-import { Config, Nav, Platform } from 'ionic-angular';
+import { Nav, Platform } from 'ionic-angular';
 
 import { FirstRunPage } from '../pages';
 import { Settings } from '../providers';
@@ -54,7 +54,7 @@ export class MyApp {
     { title: 'TradeConfirmation', component: 'TradeConfirmationPage' }
   ]
 
-  constructor(platform: Platform, settings: Settings, private config: Config, private statusBar: StatusBar, private splashScreen: SplashScreen) {
+  constructor(platform: Platform, settings: Settings, private statusBar: StatusBar, private splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
