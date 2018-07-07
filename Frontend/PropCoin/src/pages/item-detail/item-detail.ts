c54268f0d184241ee062bef444046ec1c46c207a
Unknown <studyalanchu@gmail.com>
2018-07-07 08:37:38 +0800

    Wallet Fine-tune

--- Frontend/PropCoin/src/pages/item-detail/item-detail.ts
+++ Frontend/PropCoin/src/pages/item-detail/item-detail.ts
@@ -14,5 +14,10 @@ export class ItemDetailPage {
   constructor(public navCtrl: NavController, navParams: NavParams, items: Items) {
     this.item = navParams.get('item') || items.defaultItem;
   }
-
+  gotoTrade() {
+    this.navCtrl.setRoot('HomePage', {}, {
+      animate: true,
+      direction: 'forward'
+    });
+  }
 }
